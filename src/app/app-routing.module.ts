68437c3c941b1d592a6d6d936ea15ea37839eed0
Muhammad Abdurraheem <1501918@sit.singaporetech.edu.sg>
2024-02-25 18:30:57 +0800

    routing from home page to login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: LoginComponent,
+    path: 'login'
+  }
+  ,
+  {
+    component: HomeComponent,
+    path: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
