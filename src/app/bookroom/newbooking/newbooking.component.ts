8f2f34e05b4623db4586620acd2c1804cf4d8bbc
Muhammad Abdurraheem <1501918@sit.singaporetech.edu.sg>
2024-03-31 14:39:10 +0800

    booking : blocked previous dates from being selected in new bookings

--- src/app/bookroom/newbooking/newbooking.component.ts
+++ src/app/bookroom/newbooking/newbooking.component.ts
@@ -11,6 +11,13 @@ export class NewbookingComponent {
   showTimeSlots: boolean = false;
   selectedTimeSlots: string[] = [];
   inputsFilled: boolean = false;
+  minDate: string; // Define minDate property
+
+  constructor() {
+    // Initialize minDate to the current date
+    const today = new Date();
+    this.minDate = today.toISOString().split('T')[0];
+  }
 
   isSelected(timeSlot: string): boolean {
     return this.selectedTimeSlots.includes(timeSlot);
