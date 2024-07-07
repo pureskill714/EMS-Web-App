d185ae44ee195f7e1839d672061a712dfc41057e
Muhammad Abdurraheem <1501918@sit.singaporetech.edu.sg>
2024-07-07 15:45:28 +0800

    added security to employeeinfos and able to access if not logged out

--- src/app/employeeinfo/employeeinfo.component.ts
+++ src/app/employeeinfo/employeeinfo.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-
+import { AuthService } from '../auth/auth.service';
+import { UserDataService } from './../user-data.service';
 
 @Component({
   selector: 'app-employeeinfo',
@@ -10,13 +11,39 @@ import { Router } from '@angular/router';
 })
 export class EmployeeinfoComponent implements OnInit {
 
-  retrievedEmployeeDetails: any[] = []
+  retrievedEmployeeDetails: any[] = [];
+  roleCheck: string | null = null;
 
   constructor(
     private router: Router,
-    private http: HttpClient,) {}
+    private authService: AuthService,
+    private userDataService: UserDataService,
+    private http: HttpClient,) 
+    {
+      const userData = this.authService.getUserData();
+      if (userData) {
+        this.roleCheck = userData.role;
+      }
+  
+      console.log(this.roleCheck)
+      if (this.roleCheck != "non-managerial") {
+        this.router.navigate(["not-authorized"]);
+        this.authService.kickUser(); // Call AuthService kick user method
+      }
+    }
 
     ngOnInit(): void {
+      const userData = this.authService.getUserData();
+    if (userData) {
+      const { email, firstName, lastName, role } = userData;
+      this.userDataService.setFirstName(userData.firstName);
+      this.userDataService.setLastName(userData.lastName);
+      this.userDataService.setEmail(userData.email);
+      this.userDataService.setRole(userData.role);
+    } else {
+      alert("THIS IS FAIL")
+    }
+
       this.getEmployeeDetails();
     }
 
